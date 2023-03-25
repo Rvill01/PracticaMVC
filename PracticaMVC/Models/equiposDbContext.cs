c888ccdfd586a97b72b31134a9d85d4dc8694060
rvvil <josue8289@gmail.com>
2023-03-25 12:00:25 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/Models/equiposDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using practica_25_03_2023.Models;
+
+namespace PracticaMVC.Models
+{
+    public class equiposDbContext : DbContext
+    {
+        public equiposDbContext(DbContextOptions<equiposDbContext> options) : base(options)
+        { 
+        
+        }
+        public DbSet<marcas> marcas { get; set; }
+        public DbSet<reservas> reservas { get; set; }
+        public DbSet<tipo_equipo> tipo_equipo { get; set; }
+        public DbSet<usuarios> usuarios { get; set; }
+        public DbSet<equipos> equipos { get; set; }
+        public DbSet<carreras> carreras { get; set; }
+        public DbSet<estados_equipo> estados_equipo { get; set; }
+        public DbSet<estados_reservas> estados_reserva { get; set; }
+        public DbSet<facultades> facultades { get; set; }
+    }
+}
---
+++ PracticaMVC/Models/estados_reservas.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticaMVC.Models
+{
+    public class estados_reservas
+    {
+        [Key]
+        public int estado_res_id { get; set; }
+        public string? estado { get; set; }
+    }
+}
